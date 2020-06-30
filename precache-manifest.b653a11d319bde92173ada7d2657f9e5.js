9d5b2673323bba4db89cdda3c58de80899d63036
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2020-06-30 14:54:08 -0400

    firstCommit

---
+++ precache-manifest.b653a11d319bde92173ada7d2657f9e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ea093094d4ab61d9602f505429573f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d07f84a1e4433eaacbc3",
+    "url": "./static/css/main.e1f06d53.chunk.css"
+  },
+  {
+    "revision": "10d61c320128aa1e693a",
+    "url": "./static/js/2.4930b93f.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "./static/js/2.4930b93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07f84a1e4433eaacbc3",
+    "url": "./static/js/main.bdf4a2f1.chunk.js"
+  },
+  {
+    "revision": "0ec9379679513f54ac77",
+    "url": "./static/js/runtime-main.be7ff329.js"
+  }
+]);
